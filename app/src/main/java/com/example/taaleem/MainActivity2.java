aadf163fdeb35d7c78403b1d242e4e48d5c09951
Pritam <pc032003@gmail.com>
2023-09-15 00:00:21 +0530

    dekh

--- app/src/main/java/com/example/taaleem/MainActivity2.java
+++ app/src/main/java/com/example/taaleem/MainActivity2.java
@@ -2,34 +2,28 @@ package com.example.taaleem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ClipData;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        getSupportActionBar().setTitle("Taaleem");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        textView = findViewById(R.id.textView2);
+        textView = findViewById(R.id.textView3);
+        textView = findViewById(R.id.textView4);
+
+
     }
 
-    public boolean onSupportNavigateUp(){
-        onBackPressed();
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // handle arrow click here
-        if (item.getItemId() == android.R.id.home) {
-            finish(); // close this activity and return to preview activity (if there is any)
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+   
 }
